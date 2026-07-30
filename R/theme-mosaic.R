38026fe633de14b966f7518ea8b72a4776826fdb
gklorfine <gklorfine@rogers.com>
2026-07-30 14:22:42 -0400

    Enhance `theme_mosaic()`
    - Add category label rotation (angle-aware)
    - Remove axis ticks (aligned with `vcd`)
    - Apply bold face to axis labels (aligned with `vcd`)
    - Update and fix documentation

--- R/theme-mosaic.R
+++ R/theme-mosaic.R
@@ -2,15 +2,20 @@
 #'
 #' Themes set the general aspect of the plot such as the colour of the
 #' background, gridlines, the size and colour of fonts.
-#' \code{theme_mosaic} provides access to the regular ggplot2 theme, but removes any
-#' background, most of the gridlines, and ensures an aspect ratio of 1 for better
-#' viewing of the mosaics.
+#' \code{theme_mosaic} provides access to the regular ggplot2 theme, but removes
+#' any background, axes ticks, most of the gridlines, and ensures an aspect
+#' ratio of 1 for better viewing of the mosaics. This theme also applies a bold
+#' face to axes labels and allows for the convenient rotation of category labels.
 #'
-#' @param base_size base font size
-#' @param base_family base font family
+#' @param base_size
+#'   Base font size. Defaults to 11.
+#' @param base_family
+#'   Base font family. Defaults to \code{""}, which uses the graphics device's default font family.
+#' @param rot_labels
+#'   The angle (in degrees) used to rotate category labels. Defaults to 0 degrees.
 #'
 #' @examples
-#' library(ggmosaic)
+#' library(ggmosaic2)
 #' data(happy)
 #' ggplot(data = happy) +
 #'   geom_mosaic(aes(weight=wtssall, x=product(health), fill=happy), na.rm=TRUE) +
@@ -20,12 +25,35 @@
 NULL
 #' @export
 #' @import ggplot2
-theme_mosaic <- function (base_size = 11, base_family = "")
+theme_mosaic <- function (base_size = 11, base_family = "", rot_labels = 0)
 {
-  theme_grey(base_size = base_size, base_family = base_family) %+replace%
+  # Match guide_axis() justification for bottom and left axes.
+  angle_radians <- rot_labels * pi / 180
+  cosine <- sign(round(cos(angle_radians), 3)) / 2 + 0.5
+  sine <- sign(round(sin(angle_radians), 3)) / 2 + 0.5
+
+  mosaic_theme <-
+    theme_grey(base_size = base_size, base_family = base_family) %+replace%
     theme(
+      axis.text = element_text(),
+      axis.title = element_text(face = "bold"),
+      axis.ticks = element_blank(),
       panel.grid = element_blank(),
       panel.background = element_blank(),
       aspect.ratio = 1
+    )
+
+  mosaic_theme +
+    theme(
+      axis.text.x = element_text(
+        angle = rot_labels,
+        hjust = sine,
+        vjust = cosine
+      ),
+      axis.text.y = element_text(
+        angle = rot_labels,
+        hjust = cosine,
+        vjust = 1 - sine
+      )
     )
 }
--- issues/GK-ideas.md
+++ issues/GK-ideas.md
@@ -2,12 +2,24 @@
 
 Below is an ongoing list of ideas I have for the package. I will implement these when I get the chance.
 
-[X] New package logo
+[ ] Wondering if there's a way to make category labels for > 2-way tables appear on the right and top of the display (like in `vcd`)
 
-[ ] Add a rot_labels argument to `R/theme-mosaic.R`
+## `R/theme-mosaic.R`
+
+[X] Add a `rot_labels` argument
+
+[X] Remove axes ticks (aligned w/ `vcd`)
+
+[X] Bold axes labels (aligned w/ `vcd`)
+
+## Miscellaneous
+
+[X] New package logo
 
 [ ] Lots of knitted `.html` files under `issues/`. Can most likely `.gitignore` these
 
 [ ] Should make a `CLAUDE.md` and `AGENTS.md`. Read somewhere that this is best done by hand (rather than `/init` in Claude), so will need to familiarize myself with the repo beforehand.
 
   + Could also just use the one created by `/init` for now and then replace/refine it later. Probably better than nothing.
+  
+[ ] The README example might be a bit more convoluted than need be (probably preferable to use data that doesn't need `fct_recode`; perhaps the classic `HairEyeColor`?)
